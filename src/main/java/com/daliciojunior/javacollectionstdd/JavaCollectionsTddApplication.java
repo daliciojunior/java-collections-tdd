87ae362233ee81afd51d19e106f1cac61bcde399
daliciojunior <daliciojunior@gmail.com>
2022-08-30 07:07:21 -0300

    Spring Boot starter files set up

---
+++ src/main/java/com/daliciojunior/javacollectionstdd/JavaCollectionsTddApplication.java
@@ -0,0 +1,13 @@
+package com.daliciojunior.javacollectionstdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCollectionsTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCollectionsTddApplication.class, args);
+	}
+
+}
